11a1be0bdc4de42a29554bb715b0dbf71b8f9f8a
Mikaela <mgoldric@mail.umw.edu>
2016-02-11 14:27:22 -0500

    started main()

--- lab3/texteditor.cpp
+++ lab3/texteditor.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main(){
+	using namespace std;
+	string filename;
+	cin.ignore();
+	cout << "Please enter a file name" << endl;
+	cin >> filename;
+	ifstream input_file (filename);
+	cin.ignore();
+	// append .out to input file name to create output file name
+	filename.append(".out");
+	ofstream output_file (filename);
+	return 0;
+}
