cf2094a4b1fa37a0eaf1b3b9f606c39d56868cda
Nathan <woodb4th8@aim.com>
2016-02-11 14:51:45 -0500

    text added to main()

--- lab3/texteditor.cpp
+++ lab3/texteditor.cpp
@@ -14,6 +14,16 @@ int main(){
 	// append .out to input file name to create output file name
 	filename.append(".out");
 	ofstream output_file (filename);
+	cout << "The Editing commands are:"<< endl;
+	cout << "L :Determine the length of the current line"<<endl;
+	cout << "P str :Find the position (from 0) of the string str"<<endl;
+	cout<<"I str :Insert string str"<<endl;
+	cout<<"D p n :Delete n characters beginning at position p"<<endl;
+	cout<<"R str :Replace substring str with another string"<<endl;
+	cout<<"N :Get the next line of text"<<endl;
+	cout<<"Q :quit editing"<<endl;
+	cout<<endl<<"Enter an editing command following each prompt >"<<endl;
+
 	return 0;
 	// testing comment
 }
